5694de8ef5c6432f5537ec1c4473e6f29ce9ecf3
FehDx01 <lessa.fernando01@gmail.com>
2023-12-12 21:11:02 -0300

    exercícios 1 e 2 prontos

---
+++ exercicio3/script.js
@@ -0,0 +1,8 @@
+/* const maioresPaises = ["Russia", "Canadá", "China", "EUA", "Brasil"]
+
+const ranking = maioresPaises.splice(0,5, "1 - Russia", "2 - Canadá", "3 - China", "4 - EUA", "5 - Brasil")
+
+for(let paises of maioresPaises){
+        console.log(paises);
+}
+ */
